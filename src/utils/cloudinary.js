6de0a4dfe6e68c65dbdb4baf35b497e34b13210f
Kartik-chavan51 <kartikkchavan08@gmail.com>
2025-09-13 11:47:27 +0530

    File upload to cloudinary and multer middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary=async(localFilePath)=>{
+    try{
+        if(!localFilePath) return null;
+
+        //upload to cloudinary
+        const response=await cloudinary.uploader.upload(localFilePath,
+            {
+                resource_type:"auto",//image,video,raw
+            }
+         )
+         //file has been uploaded successfully
+         console.log("file uploaded on cloudinary ",response.url);
+         return response;
+    }
+    catch(err){
+        fs.unlinkSync(localFilePath); //deleting the file from local storage
+        return null;
+    }
+}
+
+export { uploadOnCloudinary };
