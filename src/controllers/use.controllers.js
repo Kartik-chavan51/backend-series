da5c169b359d2042c5da8179cf6861997945173e
Kartik-chavan51 <kartikkchavan08@gmail.com>
2025-09-17 22:41:35 +0530

    add refresh token functionality and update routes

--- src/controllers/use.controllers.js
+++ src/controllers/use.controllers.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -152,5 +153,38 @@ const logoutUser=asyncHandler(async(req,res)=>{
         )
 })
 
-export { loginUser, logoutUser, registerUser };
+const refreshAccessToken=asyncHandler(async(req,res)=>{
+    const incomingRefreshToken=req.cookies.refreshToken||req.body.refreshToken
+
+    if(!incomingRefreshToken){
+        throw new ApiError(401,"Unothorized request");
+    }
+    try {
+        const decodeToken=jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET)
+    
+        const user=await User.findById(decodeToken?._id);
+    
+        if(!user || user?.refreshToken!==incomingRefreshToken){
+            throw new ApiError(401,"Invalid refresh token");
+        }
+    
+        const options={
+            httpOnly:true,
+            secure:true
+        }
+        const {accessToken,newrefreshToken}=await genarateAccessAndRefreshToken(user._id)
+    
+        return res.status(200)
+        .cookie("accessToken",accessToken,options)
+        .cookie("refreshToken",newrefreshToken,options)
+        .json(
+            new ApiResponse(200,{accessToken,refreshToken:newrefreshToken},"Access token refreshed successfully")
+        )
+    } catch (error) {
+        throw new ApiError(401,error?.message || "Unauthorized");
+    }
+
+})
+
+export { loginUser, logoutUser, refreshAccessToken, registerUser };
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/use.controllers.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/use.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const router= Router();
@@ -22,6 +22,8 @@ router.route('/login').post(loginUser)
 
 //secured route
 router.route('/logout').post(verifyJWT,logoutUser)
+
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 export default router; 
