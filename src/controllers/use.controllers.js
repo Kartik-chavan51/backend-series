23833c6ebb6a41d1fd608315ab9f7a435f39a180
Kartik-chavan51 <kartikkchavan08@gmail.com>
2025-09-18 11:18:28 +0530

    add getWatchHistory function to retrieve user's watch history with video owner details

--- src/controllers/use.controllers.js
+++ src/controllers/use.controllers.js
@@ -351,9 +351,62 @@ const getUserChannelprofile=asyncHandler(async(req,res)=>{
     .json(new ApiResponse(200,channel[0],"Channel profile fetched successfully"));
 })
 
+
+const getWatchHistory=asyncHandler(async(req,res)=>{
+    const user=await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1,
+                                        _id:1
+                                    }
+                                },
+                                {
+                                    $addFields:{
+                                        owner:{
+                                            $first:"$owner"
+                                        }
+                                    }
+                                },
+
+
+                            ]
+                        }
+                    }
+                ]
+            }
+        },
+    ])
+
+    res.status(200)
+    .json(new ApiResponse(200,user?.[0]?.watchHistory||[],"Watch history fetched successfully"));
+})
+
+
+
 export {
     changeCurrentPassword,
-    getCurrentUserDetails, getUserChannelprofile, loginUser,
+    getCurrentUserDetails, getUserChannelprofile, getWatchHistory, loginUser,
     logoutUser,
     refreshAccessToken,
     registerUser,
